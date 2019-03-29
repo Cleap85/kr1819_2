3fb46a0267183cef5002f05c0746e21a20991673
Cleap85 <48525218+Cleap85@users.noreply.github.com>
2019-03-29 12:22:22 +0100

    Session6

--- addition.pl
+++ addition.pl
@@ -1,7 +1,15 @@
+% Peano Arithmetic concept:
+% 
+% 0 - 0
+% 1 - n(0)
+% 2 - n(n(0))
+% 3 - n(n(n(0)))
+%
+%
 % sum(?X, ?Y, ?Z)
-% is it true if Z is the sum of X + Y.
+% is it true if Z is the sum of X + Y using Peano Arithmetic.
 
-
+% define the induction priciple:
 % 1. P(n0)
 % 2. P(n-1) -> P(n)
 
@@ -19,9 +27,9 @@
 
 % multiply(X, Y, Z)
 % it is true if Z is X * Y.
-% it is true if Z is the resultuf sum X, y times.
+% it is true if Z is the result of sum X, Y times.
 
   multiply(_, 0, 0).
  
 % multiply(n1, ....) -> multiply(n, ....)
-multipy(X, n(Y), Z2) :- multiply (X, Y, Z), sum(X, Z, Z2).
+  multipy(X, n(Y), Z2) :- multiply(X, Y, Z), sum(X, Z, Z2).
