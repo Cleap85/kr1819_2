a9f98e7f615abb18154220b062d6d85875a143f7
Cleap85 <48525218+Cleap85@users.noreply.github.com>
2019-05-08 14:37:37 +0200

    Tuesday 8.5.

--- p01-1.pl
+++ p01-1.pl
@@ -17,6 +17,35 @@
   last_but_one([_|Tail], R) :-  last_but_one(Tail, R).
   
   
-  % 1) P(n0)
-  % 2) P(n-1) -> P(n)
-  %    P(N):- N2 is N-1, P(N2),
+% 1) P(n0)
+% 2) P(n-1) -> P(n)
+%    P(N):- N2 is N-1, P(N2),
+
+% P03: Find the K´s element of a list.
+
+%  element_at(X,[_|X],1).
+%  element_at(
+ 
+% P06: Find out a palindrome.
+%
+% first part: Reverse the List.
+%
+   myReverse([], []).
+   myReverse([Head|Tail], R2) :- myReverse(Tail, R), append(R, [Head], R2).
+%
+% second part: palindrome problem.
+%
+  palindrome(L) :- reverse(L, L).
+%
+% P09: Pack consecutive elements dublicates of list  elements into sublists.
+%
+% Example:
+% pack([a,a,a,a,b,c,c,a,a,d,e,e,e,e],X).
+% X = [[a,a,a,a],[b],[c,c],[a,a],[d],[e,e,e,e]]
+
+  pack([], []).
+  
+  pack([E], [[E]]).
+  pack([H1, H1|Tail], [[H1|Hr]|Tr]) :- pack([H1|Tail], [Hr|Tr]).
+  
+  pack([H1, H2|Tail], [[H1]|R]) :- H1 \= H2, pack([H2|Tail], R).
